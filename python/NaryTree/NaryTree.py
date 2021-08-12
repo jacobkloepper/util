7af5fb4b28333dc4d29b43886e4948cb90a00997
jacobkloepper <jacob.kloepper@ea.epson.com>
2021-08-12 14:30:10 -0700

    update to NaryTree

--- python/NaryTree/NaryTree.py
+++ python/NaryTree/NaryTree.py
@@ -34,6 +34,7 @@ class NaryTreeNode():
     def add_child(self, val):
         self.children.append(NaryTreeNode(val, parent=self))
 
+# Debug helpers
 def print_tree(node):
     if (node is not None):
         if node.children:
@@ -42,16 +43,6 @@ def print_tree(node):
             print(node.get_id())
         else:
             print(node.get_id())
-
-def itemize_tree(node, scanned_tree):
-    # Note: scanned_tree must be an empty list when first passed.
-    if (node is not None):
-        if node.children:
-            for child in node.children:
-                itemize_tree(child, scanned_tree)
-            scanned_tree.append(node)
-        else:
-            scanned_tree.append(node)
 
 def print_branches(root_node, leaf_val):
     # Look through the tree, find leaf_val, then print its parents back up to root.
@@ -62,6 +53,23 @@ def print_branches(root_node, leaf_val):
 
     for leaf in leaves:
         print_to_root(leaf)
+
+def print_to_root(node):
+    print(node.get_id())
+    if (node.get_parent()):
+        print_to_root(node.get_parent())
+
+# Get containers of nodes. ...FUNC_ids() indicates that the function 
+#   returns the node ids, not the node objects.
+def itemize_tree(node, scanned_tree):
+    # Note: scanned_tree must be an empty list when first passed.
+    if (node is not None):
+        if node.children:
+            for child in node.children:
+                itemize_tree(child, scanned_tree)
+            scanned_tree.append(node)
+        else:
+            scanned_tree.append(node)
 
 def itemize_branches(root_node, leaf_val):
     # Look through the tree, find leaf_val, then print its parents back up to root.
@@ -78,12 +86,29 @@ def itemize_branches(root_node, leaf_val):
     
     return branches
 
-def print_to_root(node):
-    print(node.get_id())
-    if (node.get_parent()):
-        print_to_root(node.get_parent())
+def itemize_branch_ids(root_node, leaf_val):
+    # Look through the tree, find leaf_val, then print its parents back up to root.
+    tree = []
+    itemize_tree(root_node, tree)
 
+    branches = []
+    leaves = [node for node in tree if ((node.get_id() == leaf_val) and (not node.children))]
+
+    for leaf in leaves:
+        _tmplist = []
+        scan_to_root_ids(leaf, _tmplist)
+        branches.append(_tmplist)
+    
+    return branches
+
+# Helpers to traverse and collect from the tree. ...FUNC_ids() indicates that the function 
+#   returns the node ids, not the node objects.
 def scan_to_root(node, branch_list):
     branch_list.append(node)
     if (node.get_parent()):
         scan_to_root(node.get_parent(), branch_list)
+
+def scan_to_root_ids(node, branch_list):
+    branch_list.append(node.get_id())
+    if (node.get_parent()):
+        scan_to_root_ids(node.get_parent(), branch_list)
